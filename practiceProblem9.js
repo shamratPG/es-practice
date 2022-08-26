345da66f4d90cf6cd86c688593b7f85432d4f283
shamratPG <shamratpg@gmail.com>
2022-08-26 22:26:04 +0600

    multidimentional object

---
+++ practiceProblem9.js
@@ -0,0 +1,33 @@
+const practiceObject = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+    }
+}
+// Console the value of email
+console.log(practiceObject['email'])
+// Console the value of address
+console.log(practiceObject['address'])
+// Console the value of city
+console.log(practiceObject.address.city)
+// Console the value of lat
+console.log(practiceObject.address.geo.lat)
+// Console the value of companyname
+console.log(practiceObject.company.name)
